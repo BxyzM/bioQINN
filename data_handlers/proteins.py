d087085c4ddeedfb063ed52d60184f5e7b452d0b
Aritra <aritrabal2@gmail.com>
2025-12-04 12:27:14 +0100

    List of proteins: probably not needed, also present in YAML

---
+++ data_handlers/proteins.py
@@ -0,0 +1,13 @@
+# make a list out of it
+protein_pairs = [
+    "1TM1_E_I",
+    "3FK9_A_B",
+    #"1ID5_H_L",
+    "2IHS_A_C",
+    "3CO2_A_D",
+    "2A5Z_A_C",
+    "1PXV_A_C",
+    "1L4I_A_B",
+    "3IBM_A_B",
+    "3FUY_A_B"
+]
